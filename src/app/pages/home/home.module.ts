b34674487407e9fadb3210de22f79cdde2135322
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2024-02-22 00:09:00 -0300

    Ajustes e melhoria de design

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,11 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
 const routes: Routes = [
@@ -26,10 +22,6 @@ const routes: Routes = [
         CommonModule,
         FormsModule,
         RouterModule.forChild(routes),
-        MatIconModule,
-        MatDividerModule,
-        MatCheckboxModule,
-        MatButtonModule,
         NgxMaskDirective,
         NgxMaskPipe
     ],
