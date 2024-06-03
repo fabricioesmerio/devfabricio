629a44fe5095bfb08175c785b2afabf394058e78
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2024-06-03 18:07:29 -0300

    Cria arquivo para gerenciar menu principal

---
+++ src/app/shared/menu-data.ts
@@ -0,0 +1,17 @@
+export interface IMenu {
+    label: string;
+    routerLink: string | Array<string>;
+}
+
+export type MenuType = Array<IMenu>
+
+export const MENU_DATA: MenuType = [
+    {
+        label: 'Gerador de CNPJs',
+        routerLink: 'cnpj-generator'
+    },
+    {
+        label: 'Gerador de senhas',
+        routerLink: 'password-generator'
+    }
+]
