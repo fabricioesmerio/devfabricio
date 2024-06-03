1bf70dd4c1f5456f659f0d7ee3f6fe46f4456adb
fabricio.esmerio <fabricio.esmerio@linx.com.br>
2024-06-02 22:17:44 -0300

    Adiciona component gerador de senhas

---
+++ src/app/pages/password-generator/password-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-password-generator',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './password-generator.component.html',
+  styleUrl: './password-generator.component.scss'
+})
+export class PasswordGeneratorComponent {
+
+}
