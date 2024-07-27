aa934ab8356cc158082dd938681e38fb6b156134
fabricio.esmerio <fabricio.esmerio@linx.com.br>
2024-07-26 22:00:01 -0300

    Change routing order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
+        path: '',
+        redirectTo: 'cnpj-generator',
+        pathMatch: 'full'
+    },
+    {
         path: 'cnpj-generator',
         loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
     },
     {
         path: 'password-generator',
         loadComponent: () => import('./pages/password-generator/password-generator.component').then(m => m.PasswordGeneratorComponent)
-    },
-    {
-        path: '',
-        redirectTo: 'cnpj-generator',
-        pathMatch: 'full'
     }
 ];
 
